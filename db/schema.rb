9d908c4716391b12d3c68977307065e411631a47
imhilla <hillaryodhiambo282@gmail.com>
2020-07-08 17:54:03 +0300

    connect users to tweets

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_08_141310) do
+ActiveRecord::Schema.define(version: 2020_07_08_143359) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "tweets"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
